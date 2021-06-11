6de28c097fd7c11240e7b7b2771c9046149b4f78
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-11 14:07:58 +0300

    refactor: replace grpc-create-metadata with interval

---
+++ test/metadata.create.test.js
@@ -0,0 +1,34 @@
+const test = require('ava')
+const grpc = require('@grpc/grpc-js')
+const create = require('../lib/metadata')
+
+test('should return undefined if not-object', (t) => {
+  t.is(create('hello'), undefined)
+  t.is(create(undefined), undefined)
+})
+
+test('should return grpc.Metadata', (t) => {
+  const meta = new grpc.Metadata()
+  meta.set('hello', 'world')
+
+  const returned = create(meta)
+  t.truthy(returned instanceof grpc.Metadata)
+  t.deepEqual(meta.get('hello'), ['world'])
+})
+
+test('should handle buffers', (t) => {
+  const meta = create({ 'john-bin': Buffer.from('snow'), 'non-existing': null, john: [] }, { addEmpty: true })
+  t.deepEqual(meta.get('john-bin'), [Buffer.from('snow')])
+  t.deepEqual(meta.get('non-existing'), [])
+  t.deepEqual(meta.get('john'), [''])
+
+  const empty = create({ john: [] }, { addEmpty: false })
+  t.deepEqual(empty.get('john'), [])
+
+  try {
+    create({ john: Buffer.from('snow') })
+    t.fail()
+  } catch (error) {
+    t.is(error.message, 'Metadata key "john" should end with "-bin" since it has a non-string value.')
+  }
+})
