568464cbd48729c735d5ca9d4657e6e58d1f23a7
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-14 11:55:05 +0300

    test: add mali.use to benchmarks

--- test/benchmark.js
+++ test/benchmark.js
@@ -22,5 +22,14 @@ suite
     const app = new Mali()
     app.addService(path.join(__dirname, './protos/helloworld.proto'), 'Greeter')
   })
+  .add('mali.use', () => {
+    const app = new Mali()
+    app.addService(path.join(__dirname, './protos/helloworld.proto'), 'Greeter')
+    app.use({
+      SayHello: (ctx) => {
+        ctx.res = { message: `Hi ${ctx.name}!` }
+      }
+    })
+  })
   .on('cycle', (event) => console.log(event.target.toString()))
   .run({ async: true })
